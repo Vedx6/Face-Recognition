2b9e73d3d937c197da48f42e1b0ed6a8c18c0db2
Vedx6 <vedanttt06@gmail.com>
2025-08-25 17:32:50 +0530

    Added requirements.txt and updated files for Render deployment

--- 4_Flask_App/main.py
+++ 4_Flask_App/main.py
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-from flask import Flask # type: ignore
+from flask import Flask  # type: ignore
 from app import views
 
-app = Flask(__name__) # webserver gateway interphase (WSGI)
+app = Flask(__name__)  # webserver gateway interface (WSGI)
 
-app.add_url_rule(rule='/',endpoint='home',view_func=views.index)
-app.add_url_rule(rule='/app/',endpoint='app',view_func=views.app)
-app.add_url_rule(rule='/app/gender/',
+# Routes
+app.add_url_rule(rule='/', endpoint='home', view_func=views.index)
+app.add_url_rule(rule='/app/', endpoint='app', view_func=views.app)
+app.add_url_rule(rule='/app/gender/', 
                  endpoint='gender',
                  view_func=views.genderapp,
-                 methods=['GET','POST'])
+                 methods=['GET', 'POST'])
 
 if __name__ == "__main__":
-=======
-from flask import Flask
-from app import views
-
-app = Flask(__name__) # webserver gateway interphase (WSGI)
-
-app.add_url_rule(rule='/',endpoint='home',view_func=views.index)
-app.add_url_rule(rule='/app/',endpoint='app',view_func=views.app)
-app.add_url_rule(rule='/app/gender/',
-                 endpoint='gender',
-                 view_func=views.genderapp,
-                 methods=['GET','POST'])
-
-if __name__ == "__main__":
->>>>>>> 795f1e101b30f90cf3dceed5d742bb8375e6f712
     app.run(debug=True)
